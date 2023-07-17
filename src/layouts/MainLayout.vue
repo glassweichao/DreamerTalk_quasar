41774ac31c6257647f085ffea21df45fdbcb7ac3
Charles Wei <kg_terry@126.com>
2023-07-17 23:57:44 +0800

    修改调音台为变形弹出模式

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,43 +1,158 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
+  <q-layout view="hHh lpR fFf">
+    <!-- 头部-头像、ID、网络状态等 -->
+    <q-header class="transparent text-white q-pa-md">
+      <q-chip outline>
+        <q-avatar color="primary"> C </q-avatar>
+        Charles Wei
+      </q-chip>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+    <q-footer class="transparent text-white">
+      <!-- 调音台 -->
+      <div class="row justify-center items-center q-pa-md">
+        <div class="col-4 text-center relative-position">
+          <q-btn
+            v-morph:btn_equalizer:mygroup:300.resize="morphGroupModel"
+            round
+            color="primary"
+            icon="equalizer"
+            @click="nextMorph"
+          />
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+          <q-card
+            v-morph:card_audio_manage:mygroup:500.resize="morphGroupModel"
+            class="absolute-bottom-left q-ma-lg text-blue text-left"
+            style="width: 300px"
+          >
+            <!-- 调音台 -->
+            <q-card-section>
+              <q-checkbox
+                class="text-primary"
+                label="声音增强"
+                v-model="check1"
+              />
+              <q-checkbox
+                class="text-primary"
+                label="降噪增强"
+                v-model="check2"
+              />
+              <q-checkbox
+                class="text-primary"
+                label="回声消除增强"
+                v-model="check3"
+              />
+            </q-card-section>
+
+            <q-item class="q-ma-sm">
+              <q-item-section side>
+                <q-icon color="teal" name="volume_down" />
+              </q-item-section>
+              <q-item-section>
+                <q-slider
+                  v-model="volume"
+                  :min="0"
+                  :max="100"
+                  label
+                  color="teal"
+                />
+              </q-item-section>
+            </q-item>
+
+            <q-item class="q-ma-sm">
+              <q-item-section side>
+                <q-icon color="primary" name="mic" />
+              </q-item-section>
+              <q-item-section>
+                <q-slider v-model="mic" :min="0" :max="100" label />
+              </q-item-section>
+            </q-item>
+
+            <q-card-actions align="right">
+              <q-btn flat label="试麦" @click="nextMorph" />
+              <q-btn flat label="关闭" @click="cancelMorph" />
+            </q-card-actions>
+          </q-card>
+
+          <q-card
+            v-morph:card_dev_manage:mygroup:500.tween="morphGroupModel"
+            class="absolute-bottom-left q-ma-lg text-primary"
+            style="width: 300px"
+          >
+            <q-card-section>
+              <q-btn-dropdown
+                color="pink"
+                label="Dropdown Button"
+                icon="mic"
+                dropdown-icon="mic"
+              >
+                <q-list>
+                  <q-item clickable v-close-popup @click="onItemClick">
+                    <q-item-section>
+                      <q-item-label>麦克风设备</q-item-label>
+                    </q-item-section>
+                  </q-item>
+                </q-list>
+              </q-btn-dropdown>
+            </q-card-section>
+
+            <q-card-section>
+              <q-btn-dropdown
+                color="pink"
+                label="Dropdown Button"
+                icon="volume_up"
+                dropdown-icon="volume_up"
+              >
+                <q-list>
+                  <q-item clickable v-close-popup @click="onItemClick">
+                    <q-item-section>
+                      <q-item-label>扬声器设备</q-item-label>
+                    </q-item-section>
+                  </q-item>
+                </q-list>
+              </q-btn-dropdown>
+            </q-card-section>
+
+            <q-card-section>
+              <div class="row items-center q-ma-md">
+                <div class="col-4">
+                  <q-btn label="开始试麦" size="sm" />
+                </div>
+                <div class="col-8">
+                  <q-linear-progress
+                    rounded
+                    size="15px"
+                    :value="0.5"
+                    color="teal"
+                  />
+                </div>
+              </div>
+            </q-card-section>
+
+            <q-card-actions align="right">
+              <q-btn flat label="关闭" @click="cancelMorph" />
+            </q-card-actions>
+          </q-card>
+        </div>
+
+        <div class="col-4 text-center">
+          <q-btn
+            round
+            color="primary"
+            :icon="micOn ? 'mic' : 'mic_off'"
+            @click="micOn = !micOn"
+          />
+        </div>
+        <div class="col-4 text-center">
+          <q-btn
+            round
+            color="teal"
+            :icon="speakerOn ? 'volume_up' : 'volume_off'"
+            @click="speakerOn = !speakerOn"
+          />
+        </div>
+      </div>
+    </q-footer>
 
     <q-page-container>
       <router-view />
@@ -46,71 +161,46 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { defineComponent, ref } from "vue";
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+const audioManageStep = {
+  btn_equalizer: "card_audio_manage",
+  card_audio_manage: "card_dev_manage",
+};
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
-  components: {
-    EssentialLink
+  data() {
+    return {
+      micOn: true,
+      speakerOn: true,
+    };
   },
 
-  setup () {
-    const leftDrawerOpen = ref(false)
-
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const morphGroupModel = ref("btn_equalizer");
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+      check1: ref(true),
+      check2: ref(false),
+      check3: ref(false),
+
+      volume: ref(80),
+      mic: ref(80),
+
+      morphGroupModel,
+      nextMorph() {
+        morphGroupModel.value = audioManageStep[morphGroupModel.value];
+      },
+      cancelMorph() {
+        morphGroupModel.value = "btn_equalizer";
+      },
+    };
+  },
+});
 </script>
--- src/pages/CustomMainWindow.vue
+++ src/pages/CustomMainWindow.vue
@@ -1,204 +1,99 @@
 <template>
-  <q-layout view="hHh lpR fFf">
-    <!-- 头部-头像、ID、网络状态等 -->
-    <q-header class="transparent text-white q-pa-md">
-      <q-chip outline>
-        <q-avatar color="primary"> C </q-avatar>
-        Charles Wei
-      </q-chip>
-    </q-header>
+  <div class="q-pa-md">
+    <q-btn-dropdown
+      class="full-width"
+      color="primary"
+      label="Room List"
+      rounded
+      size="18px"
+      unelevated
+      @show="refleshRoomList"
+    >
+      <q-list>
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section>
+            <q-item-label>Photos</q-item-label>
+          </q-item-section>
+        </q-item>
 
-    <!-- 调音台 -->
-    <q-drawer v-model="leftDrawerOpen" side="left" overlay bordered>
-      <!-- drawer content -->
-      <div class="row justify-end">
-        <div class="row-1 justify-end">
-          <q-btn label="x" text-color="red" flat="" round @click="leftDrawerOpen = false" />
-        </div>
-        <div class="fit">
-          <!-- 调音台 -->
-          <q-item tag="label" v-ripple>
-            <q-item-section side top>
-              <q-checkbox v-model="check1" />
-            </q-item-section>
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section>
+            <q-item-label>Videos</q-item-label>
+          </q-item-section>
+        </q-item>
 
-            <q-item-section>
-              <q-item-label>麦克风增强</q-item-label>
-            </q-item-section>
-          </q-item>
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section>
+            <q-item-label>Articles</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
+  </div>
 
-          <q-item tag="label" v-ripple>
-            <q-item-section side top>
-              <q-checkbox v-model="check2" />
-            </q-item-section>
+  <div class="q-pa-md">
+    <q-list flat bordered>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white"> R </q-avatar>
+        </q-item-section>
 
-            <q-item-section>
-              <q-item-label>变声</q-item-label>
-            </q-item-section>
-          </q-item>
+        <q-item-section> 一剑宛春秋 </q-item-section>
 
-          <q-item>
-            <q-item-section side>
-              <q-icon color="teal" name="volume_down" />
-            </q-item-section>
-            <q-item-section>
-              <q-slider
-                v-model="volume"
-                :min="0"
-                :max="100"
-                label
-                color="teal"
-              />
-            </q-item-section>
-          </q-item>
+        <q-item-section side>
+          <q-badge rounded color="green" />
+        </q-item-section>
+      </q-item>
 
-          <q-item>
-            <q-item-section side>
-              <q-icon color="primary" name="mic" />
-            </q-item-section>
-            <q-item-section>
-              <q-slider v-model="mic" :min="0" :max="100" label />
-            </q-item-section>
-          </q-item>
-        </div>
-      </div>
-    </q-drawer>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white"> R </q-avatar>
+        </q-item-section>
 
-    <q-page-container>
-      <div class="q-pa-md">
-        <q-btn-dropdown
-          class="full-width"
-          color="primary"
-          label="Room List"
-          rounded
-          size="18px"
-          unelevated
-          @show="refleshRoomList"
-        >
-          <q-list>
-            <q-item clickable v-close-popup @click="onItemClick">
-              <q-item-section>
-                <q-item-label>Photos</q-item-label>
-              </q-item-section>
-            </q-item>
+        <q-item-section> 一剑宛春秋 </q-item-section>
 
-            <q-item clickable v-close-popup @click="onItemClick">
-              <q-item-section>
-                <q-item-label>Videos</q-item-label>
-              </q-item-section>
-            </q-item>
+        <q-item-section side>
+          <q-badge rounded color="green" />
+        </q-item-section>
+      </q-item>
 
-            <q-item clickable v-close-popup @click="onItemClick">
-              <q-item-section>
-                <q-item-label>Articles</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-btn-dropdown>
-      </div>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white"> R </q-avatar>
+        </q-item-section>
 
-      <div class="q-pa-md">
-        <q-list flat bordered>
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-avatar color="primary" text-color="white"> R </q-avatar>
-            </q-item-section>
+        <q-item-section> 一剑宛春秋 </q-item-section>
 
-            <q-item-section> 一剑宛春秋 </q-item-section>
+        <q-item-section side>
+          <q-badge rounded color="green" />
+        </q-item-section>
+      </q-item>
 
-            <q-item-section side>
-              <q-badge rounded color="green" />
-            </q-item-section>
-          </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white"> R </q-avatar>
+        </q-item-section>
 
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-avatar color="primary" text-color="white"> R </q-avatar>
-            </q-item-section>
+        <q-item-section> 一剑宛春秋 </q-item-section>
 
-            <q-item-section> 一剑宛春秋 </q-item-section>
+        <q-item-section side>
+          <q-badge rounded color="green" />
+        </q-item-section>
+      </q-item>
 
-            <q-item-section side>
-              <q-badge rounded color="green" />
-            </q-item-section>
-          </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white"> R </q-avatar>
+        </q-item-section>
 
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-avatar color="primary" text-color="white"> R </q-avatar>
-            </q-item-section>
+        <q-item-section> 一剑宛春秋 </q-item-section>
 
-            <q-item-section> 一剑宛春秋 </q-item-section>
-
-            <q-item-section side>
-              <q-badge rounded color="green" />
-            </q-item-section>
-          </q-item>
-
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-avatar color="primary" text-color="white"> R </q-avatar>
-            </q-item-section>
-
-            <q-item-section> 一剑宛春秋 </q-item-section>
-
-            <q-item-section side>
-              <q-badge rounded color="green" />
-            </q-item-section>
-          </q-item>
-
-          <q-item clickable v-ripple>
-            <q-item-section avatar>
-              <q-avatar color="primary" text-color="white"> R </q-avatar>
-            </q-item-section>
-
-            <q-item-section> 一剑宛春秋 </q-item-section>
-
-            <q-item-section side>
-              <q-badge rounded color="green" />
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </div>
-    </q-page-container>
-
-    <q-footer class="transparent text-white">
-      <div class="row justify-center items-center q-pa-md">
-        <div class="col-4 text-center">
-          <q-btn
-            round
-            color="primary"
-            icon="equalizer"
-            @click="leftDrawerOpen = !leftDrawerOpen"
-          />
-          <!-- <q-btn-dropdown
-            persistent
-            rounded
-            color="deep-orange"
-            dropdown-icon="equalizer"
-            no-icon-animation
-          >
-          </q-btn-dropdown> -->
-        </div>
-        <div class="col-4 text-center">
-          <q-btn
-            round
-            color="deep-orange"
-            :icon="micOn ? 'mic' : 'mic_off'"
-            @click="micOn = !micOn"
-          />
-        </div>
-        <div class="col-4 text-center">
-          <q-btn
-            round
-            color="deep-orange"
-            :icon="speakerOn ? 'volume_up' : 'volume_off'"
-            @click="speakerOn = !speakerOn"
-          />
-        </div>
-      </div>
-    </q-footer>
-  </q-layout>
+        <q-item-section side>
+          <q-badge rounded color="green" />
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
 </template>
 
 <script>
@@ -206,26 +101,11 @@ import { ref } from "vue";
 
 export default {
   data() {
-    return {
-      micOn: true,
-      speakerOn: true,
-    };
+    return {};
   },
 
   setup() {
-    const leftDrawerOpen = ref(false);
-    return {
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
-      check1: ref(true),
-      check2: ref(false),
-      check3: ref(false),
-
-      volume: ref(80),
-      mic: ref(80),
-    };
+    return {};
   },
 };
 </script>
